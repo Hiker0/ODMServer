9ff675c700f695c1521eef7b92f5b33a11a1d706
Hiker0 <zhouxufeng2012@gmail.com>
2014-04-29 14:14:43 +0800

    init project

---
+++ src/com/odm/odmserver/hall/floatmenu/AirButtonGlobalMenuBgImageView.java
@@ -0,0 +1,144 @@
+package com.odm.odmserver.hall.floatmenu;
+
+import android.animation.Animator;
+import android.animation.ObjectAnimator;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.graphics.Region;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.odm.odmserver.R;
+
+public class AirButtonGlobalMenuBgImageView extends ImageView
+{
+
+
+private final int ANIMATION_DURATION = 600;
+  private final float ARC_BASE_ANGLE = 80.0F;
+  private final float ARC_MAX_ANGLE = -223.0F;
+  private final String TAG = "AirButtonGlobalMenuBgImageView";
+  private ObjectAnimator mArcAnimator = null;
+  private Path mArcPath = null;
+  private Context mContext = null;
+  private float mCurrentAnimatingArc =ARC_MAX_ANGLE;
+  private RectF mImageBoundary = null;
+  private float mImageCenterX = 0.0F;
+  private float mImageCenterY = 0.0F;
+
+
+  public AirButtonGlobalMenuBgImageView(Context context)
+  {
+    super(context);
+    this.mContext = context;
+    super.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    this.mArcAnimator = new ObjectAnimator();
+    this.mArcAnimator.setTarget(this);
+    this.mArcAnimator.setPropertyName("animationArc");
+  }
+  
+  public AirButtonGlobalMenuBgImageView(Context context, AttributeSet attrs) {
+	  
+
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+		
+	    this.mContext = context;
+	    super.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+	    super.setImageResource(R.drawable.airbutton_global_bg);
+	    this.mArcAnimator = new ObjectAnimator();
+	    this.mArcAnimator.setTarget(this);
+	    this.mArcAnimator.setPropertyName("animationArc");
+	}
+  
+  public AirButtonGlobalMenuBgImageView(Context context, AttributeSet attrs,int x) {
+	  
+
+		super(context, attrs,x);
+		// TODO Auto-generated constructor stub
+		
+	    this.mContext = context;
+	    super.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+	    super.setImageResource(R.drawable.airbutton_global_bg);
+	    this.mArcAnimator = new ObjectAnimator();
+	    this.mArcAnimator.setTarget(this);
+	    this.mArcAnimator.setPropertyName("animationArc");
+	}
+  
+
+  private void initVariables()
+  {
+    if (this.mImageBoundary == null)
+      this.mImageBoundary = new RectF(-100.0F, -100.0F, 100 + super.getWidth(), 100 + super.getWidth());
+    float f = super.getWidth() / 2;
+    this.mImageCenterY = f;
+    this.mImageCenterX = f;
+    this.mArcPath = new Path();
+    Log.v("airbutton", "AirButtonGlobalMenuBgImageView initVariables mImageCenterY=" + this.mImageCenterY + ", mImageCenterX=" + this.mImageCenterX);
+    Log.v("airbutton", "AirButtonGlobalMenuBgImageView initVariables super.getWidth()=" + super.getWidth());
+  }
+
+  protected void onDraw(Canvas paramCanvas)
+  {
+    if ((this.mImageBoundary == null) || (this.mArcPath == null)){
+    	return;
+    }
+      
+      this.mArcPath.reset();
+      this.mArcPath.moveTo(this.mImageCenterX, this.mImageCenterY);
+      this.mArcPath.arcTo(this.mImageBoundary, ARC_BASE_ANGLE, this.mCurrentAnimatingArc);
+      this.mArcPath.close();
+      paramCanvas.clipPath(this.mArcPath, Region.Op.DIFFERENCE);
+      super.onDraw(paramCanvas);
+
+  }
+
+  public void setAnimationArc(float paramFloat)
+  {
+    this.mCurrentAnimatingArc = paramFloat;
+    super.invalidate();
+  }
+
+  public void startCloseAnimation(int paramInt, Animator.AnimatorListener paramAnimatorListener)
+  {
+    initVariables();
+    Log.v("airbutton", "startCloseAnimation");
+    if (this.mArcAnimator.isRunning())
+      this.mArcAnimator.cancel();
+    this.mArcAnimator.setStartDelay(paramInt);
+    this.mArcAnimator.setDuration(ANIMATION_DURATION);
+    ObjectAnimator localObjectAnimator = this.mArcAnimator;
+    float[] arrayOfFloat = new float[2];
+    arrayOfFloat[0] = 0.0F;
+    arrayOfFloat[1] = -303.0F;
+    localObjectAnimator.setFloatValues(arrayOfFloat);
+    this.mArcAnimator.removeAllListeners();
+    if (paramAnimatorListener != null)
+      this.mArcAnimator.addListener(paramAnimatorListener);
+    this.mArcAnimator.start();
+  }
+
+  public void startOpenAnimation()
+  {
+    initVariables();
+    if (this.mArcAnimator.isRunning())
+      this.mArcAnimator.cancel();
+    this.mArcAnimator.setStartDelay(0L);
+    this.mArcAnimator.setDuration(ANIMATION_DURATION);
+    ObjectAnimator localObjectAnimator = this.mArcAnimator;
+    float[] arrayOfFloat = new float[2];
+    arrayOfFloat[0] = -303.0F;
+    arrayOfFloat[1] = 0.0F;
+    localObjectAnimator.setFloatValues(arrayOfFloat);
+    this.mArcAnimator.start();
+  }
+}
+
+/* Location:           D:\testapk\decode\dex2jar-0.0.9.9\Samsung_dex2jar.jar
+ * Qualified Name:     android.samsung.android.airbutton.view.AirButtonGlobalMenuBgImageView
+ * JD-Core Version:    0.5.4
+ */
---
+++ src/com/odm/odmserver/hall/floatmenu/MenuReceiver.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.odm.odmserver.hall.floatmenu;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import java.io.FileReader;
+import android.util.Log;
+
+public class MenuReceiver extends BroadcastReceiver {
+	static final String TAG = "FloatMenu";
+    static final String SPEN_ACTION = "huaqi.intent.action.HALL_SPEN_S";
+    static final String SPEN_LAUNCH_ACTION = "android.intent.action.floatmenu_s";
+    static final String SPEN_KEY = "hall_spen_state";
+    static final int SPEN_PUSH = 0;
+    static final int SPEN_PULL = 1;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    	Log.d(TAG,">>>FloatMenuReceiver onReceive");
+        Intent floatMenuIntent= new Intent(SPEN_LAUNCH_ACTION);
+
+        if (intent != null && intent.getAction().equals(SPEN_ACTION)) {
+            if (getHallPenState() == SPEN_PUSH) {
+                context.stopService(floatMenuIntent);
+            } else if (getHallPenState() == SPEN_PULL) {
+                context.startService(floatMenuIntent);
+            }
+        }
+   
+    }
+    private int getHallPenState() {
+	  int state = 0;
+	  String pinStateFilePath = String.format("/sys/devices/platform/Accdet_Driver/driver/tablet_state");
+		try{
+			FileReader fw = new FileReader(pinStateFilePath);
+			state = fw.read();
+			int pin_state = Integer.valueOf(state);
+			fw.close();
+			pin_state = pin_state%2;
+			Log.d(TAG, "getHallPenState PIN state for Hall is " + pin_state);
+			return pin_state;
+		} catch (Exception e) {
+			Log.d(TAG, "getHallPenState PIN state for Hall is default");
+		}	
+		return 0;
+
+    }
+
+}
