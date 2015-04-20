80caae027d2f937d79bfb8b319e3d78e8b14f9dc
Hiker0 <zhouxufeng2012@gmail.com>
2015-04-20 14:28:31 +0800

    horizontal protector

--- src/com/odm/odmserver/UpdateMonitorCallback.java
+++
@@ -1,9 +0,0 @@
-package com.odm.odmserver;
-
-public class UpdateMonitorCallback {
-
-	public void onScreenTurnedOff() { }
-    public void onScreenTurnedOn() { }
-    public void onPreferenceChanged() { }
-    
-}
